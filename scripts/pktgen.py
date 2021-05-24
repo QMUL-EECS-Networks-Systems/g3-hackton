ebc1e329f7e983f8b6cb52df942aec87409998c3
Jonatan Langlet <jonatanlanglet@gmail.com>
2021-05-24 21:15:41 +0100

    Add RoCEv2 packet generation script

---
+++ scripts/pktgen.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+#from scapy.all import send, IP, ICMP
+from scapy.all import *
+#from scapy.contrib import roce
+import random
+import sys
+import struct
+
+#dstMAC = "56:2B:95:DB:33:39"
+dstMAC = "b8:ce:f6:61:a0:f6"
+#dstMAC = "ff:ff:ff:ff:ff:ff"
+#dstMAC = "b8:ce:f6:61:9f:96" #host
+
+srcIP = "11.11.11.1"
+dstIP = "11.11.11.2"
+
+
+class BTH(Packet):
+	name = "Infiniband BTH"
+	fields_desc = [
+		ByteField("opcode", 0),
+		BitField("solicitedEvent", 0, 1),
+		BitField("migReq", 0, 1),
+		BitField("padCount", 0, 2),
+		BitField("transportHeaderVersion", 0, 4),
+		XShortField("partitionKey", 0),
+		XByteField("reserved1", 0),
+		ThreeBytesField("destinationQP", 0),
+		BitField("ackRequest", 0, 1),
+		BitField("reserved2", 0, 7),
+		ThreeBytesField("packetSequenceNumber", 0)
+	]
+
+class RETH(Packet):
+	name = "RDMA RETH"
+	fields_desc = [
+		IntField("virtualAddress", 0),
+		LongField("rKey", 0),
+		IntField("dmaLength", 0)
+		
+	]
+
+class iCRC(Packet):
+	name = "iCRC"
+	fields_desc = [
+		IntField("iCRC", 0),
+		
+	]
+
+#Make RDMA write packet with 32bit payload
+def makeRocev2Write(payload=0xdeadbeef):
+	partitionKey = 0
+	destinationQP = 0
+	dmaLength = 32
+	virtualAddress = 0 #Start of buffer
+	rKey = 0 #Kinda like the password
+	
+	iCRC_checksum = 0 #TODO: calculate this? Or ignore?
+	
+	payload = struct.pack(">I", payload)
+	
+	pkt = Ether(src="b8:ce:f6:61:a0:f2",dst=dstMAC)
+	pkt = pkt/IP(src=srcIP,dst=dstIP,ihl=5,flags=0b010,proto=0x11)
+	pkt = pkt/UDP(sport=0xc0de,dport=4791,chksum=0)
+	pkt = pkt/BTH(opcode=0b01010,partitionKey=partitionKey,destinationQP=destinationQP) #WRITE-ONLY
+	pkt = pkt/RETH(dmaLength=dmaLength,virtualAddress=virtualAddress,rKey=rKey)
+	pkt = pkt/Raw(payload)
+	pkt = pkt/iCRC(iCRC=iCRC_checksum)
+	
+	return pkt
+
+
+def makeIPPacket():
+	pkt = Ether(src="b8:ce:f6:61:a0:f2",dst=dstMAC)
+	pkt = pkt/IP(src=srcIP,dst=dstIP)
+	return pkt
+
+
+def makeUDPPacket():
+	pkt = Ether(src="b8:ce:f6:61:a0:f2",dst=dstMAC)
+	pkt = pkt/IP(src=srcIP,dst=dstIP)/UDP()
+	return pkt
+
+
+pkt = makeIPPacket()
+
+print("Sending packet", pkt)
+
+wrpcap("rocev2_pkt.pcap",pkt)
+sendp(pkt, iface="enp27s0f0")
+
